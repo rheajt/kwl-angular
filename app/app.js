87bd4aed3c9df0977b62a356e0bf8efc1598bf30
jordan rhea <rheajt@gmail.com>
2016-12-08 16:30:24 +0300

    updated firebase and start migrating to new idea

--- app/app.js
+++ app/app.js
@@ -1,19 +1,19 @@
-var app = angular.module('kwlApp', ['ngRoute', 'firebase']);
+var app = angular.module('firecharts', ['ngRoute', 'firebase']);
 
 app.constant('FBURL', 'https://kwlcharts.firebaseio.com/');
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'app/components/index/indexView.html',
+      templateUrl: 'app/views/index.view.html',
       controller: 'IndexCtrl'
     })
-    .when('/kwl/:id', {
-      templateUrl: 'app/components/kwl/kwlView.html',
+    .when('/firechart/:id', {
+      templateUrl: 'app/views/kwl.view.html',
       controller: 'KwlCtrl'
     })
     .when('/new', {
-      templateUrl: 'app/components/newchart/newchartView.html',
+      templateUrl: 'app/views/new.views.html',
       controller: 'NewchartCtrl'
     })
     .otherwise({
@@ -22,86 +22,24 @@ app.config(['$routeProvider', function($routeProvider) {
 
 }]);
 
-app.factory('Auth', ['FBURL', '$firebaseAuth', function(FBURL, $firebaseAuth) {
-  var ref = new Firebase(FBURL);
-  var auth = $firebaseAuth(ref);
-  var authData = {};
+app.factory('Auth', ['$firebaseAuth', function($firebaseAuth) {
+  return $firebaseAuth();
+}])
 
-  auth.$onAuth(function(data) {
-    if(data) {
-      authData = data;
-    }
+app.controller('IndexCtrl', ['$scope', 'Auth', function($scope, Auth) {
+
+  $scope.auth = Auth;
+
+  $scope.auth.$onAuthStateChanged(function(user) {
+    $scope.user = user;
   });
 
-  return {
-    auth: auth,
-    authData: authData
-  }
 }]);
 
-app.service('Charts', ['FBURL', '$firebaseArray', '$firebaseObject', function(FBURL, $firebaseArray, $firebaseObject) {
-  var ref = new Firebase(FBURL + '/charts');
-
-  this.ref = ref;
-
-  this.getCharts = function() {
-    return $firebaseArray(ref);
-  }
-
-  this.getChartById = function(id) {
-    return $firebaseObject(ref.child(id).child('concepts'));
-  }
-
-  this.setChart = function(chart) {
-    $firebaseArray(ref).$add(chart);
-  }
+app.controller('KwlCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
 
 }]);
 
-app.controller('IndexCtrl', ['$scope', 'Auth', 'Charts', function($scope, Auth, Charts) {
-
-  $scope.charts = Charts.getCharts();
-
-}]);
-
-app.controller('KwlCtrl', ['$scope', '$routeParams', 'Charts',
-  function($scope, $routeParams, Charts) {
-
-    $scope.chart = Charts.getChartById($routeParams.id);
-    $scope.current = 0;
-
-    Charts.ref.child($routeParams.id).child('concepts').once('value', function(snapshot) {
-      $scope.chartLength = snapshot.numChildren();
-    });
-
-    var kwl = {"know": [], "want": [], "learn": []};
-    $scope.showKwl = {"know": "", "want": "", "learn": ""};
-
-    $scope.add = function(key, data) {
-      if($scope.current < $scope.chartLength) {
-        kwl[key].push(data);
-        $scope.showKwl[key] = kwl[key].join(', ');
-        $scope.current++;
-      }
-    }
-
-    $scope.save = function() {
-      $scope.saved = 'not really saved anywhere!';
-    }
-
-}]);
-
-app.controller('NewchartCtrl', ['$scope', 'Charts', function($scope, Charts) {
-  $scope.chart = {};
-  $scope.chart.concepts = [];
-
-  $scope.addConcept = function() {
-    $scope.chart.concepts.push($scope.concept);
-    $scope.concept = '';
-  }
-
-  $scope.createChart = function() {
-    Charts.setChart($scope.chart);
-  }
+app.controller('NewchartCtrl', ['$scope', function($scope) {
 
 }])
